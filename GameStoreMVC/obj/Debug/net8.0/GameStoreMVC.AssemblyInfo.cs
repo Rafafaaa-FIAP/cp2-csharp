4f604e067082bb510f444a4dafde8ebdcf32337f
= <=>
2026-04-27 22:11:22 -0300

    ajustes layout game

--- GameStoreMVC/obj/Debug/net8.0/GameStoreMVC.AssemblyInfo.cs
+++ GameStoreMVC/obj/Debug/net8.0/GameStoreMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStoreMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4e54096a46a9f16bbb34bc451c55b072825a9a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3617a794ee82d649fb0d4f4f0760b6155c42f1b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStoreMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStoreMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
